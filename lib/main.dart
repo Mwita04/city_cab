741de64e693667771e4ff125848ec8cf1eb6f1de
Mwita04 <emcee779@gmail.com>
2022-04-14 17:42:28 +0300

    connect-firebase-android

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: await_only_futures, avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,6 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -36,6 +40,15 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
-    return Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await FirebaseFirestore.instance
+              .collection('user')
+              .add({"id": "MyId"});
+          print('Done');
+        },
+      ),
+    );
   }
 }
